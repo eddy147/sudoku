6c6b8e7e014362623c85ee6935d8ca5148389229
eddy.de.boer <eddy.de.boer@medicore.nl>
2018-10-31 13:55:27 +0100

    master: Logical & brute force sudoku

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
+                 [org.clojure/core.logic "0.8.11"]]
   :main ^:skip-aot sudoku.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- test/sudoku/core_test.clj
+++ test/sudoku/core_test.clj
@@ -2,6 +2,24 @@
   (:require [clojure.test :refer :all]
             [sudoku.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def b '[3 - - - - 5 - 1 -
+         - 7 - - - 6 - 3 -
+         1 - - - 9 - - - -
+         7 - 8 - - - - 9 -
+         9 - - 4 - 8 - - 2
+         - 6 - - - - 5 - 1
+         - - - - 4 - - - 6
+         - 4 - 7 - - - 2 -
+         - 2 - 6 - - - - 3])
+
+(deftest test-row-for
+  (testing "test gtting a row of a board."
+    (is (= '(9 - - 4 - 8 - - 2) (row-for b 4 9)))))
+
+(deftest test-col-for
+  (testing "test gtting a col of a board."
+    (is (= '(3 - 1 7 9 - - - -) (col-for b 0 9)))))
+
+(deftest test-subgrid-for
+  (testing "test getting a subgrid of a board."
+    (is (= '(- - 6 - 2 - - - 3) (subgrid-for b 79)))))
